d12140d1ede9a7c785de71edc4fce328907a3d48
dogabeey@gmail.com <dogabeey@gmail.com>
2018-10-06 18:09:09 +0300

    Add critical strike values

--- Classes/classClass.lua
+++ Classes/classClass.lua
@@ -1,7 +1,7 @@
 Class = {}
 Class.__index = Class
 
-function Class:Create(name, bonus_roll_att,bonus_roll_def)
+function Class:Create(name, bonus_roll_att, bonus_roll_def,crit_damage,crit_chance)
    local class = {}             -- our new object
    setmetatable(class,Class)  -- make Class handle lookup
    class.name = name
@@ -9,6 +9,8 @@ function Class:Create(name, bonus_roll_att,bonus_roll_def)
    class.bonus_roll_def = bonus_roll_def
    class.roll_multiplier_att = roll_multiplier_att
    class.roll_multiplier_def = roll_multiplier_def
+   class.crit_damage = crit_damage or DEF_CRIT_DAMAGE
+   class.crit_chance = crit_chance or DEF_CRIT_CHANCE
    --print("Created a class named " .. name .. " which has +" .. bonus_roll_att .. " bonus roll while attacking and +" ..  bonus_roll_def .. " bonus roll while defending.")
    return class
 end
--- Classes/raceClass.lua
+++ Classes/raceClass.lua
@@ -1,7 +1,7 @@
 Race = {}
 Race.__index = Race
 
-function Race:Create(name, bonus_roll_att,bonus_roll_def)
+function Race:Create(name, bonus_roll_att,bonus_roll_def,crit_damage,crit_chance)
    local race = {}             -- our new object
    setmetatable(race,Race)  -- make Race handle lookup
    race.name = name
@@ -9,6 +9,8 @@ function Race:Create(name, bonus_roll_att,bonus_roll_def)
    race.bonus_roll_def = bonus_roll_def
    race.roll_multiplier_att = roll_multiplier_att
    race.roll_multiplier_def = roll_multiplier_def
+   race.crit_damage = crit_damage or DEF_CRIT_DAMAGE
+   race.crit_chance = crit_chance or DEF_CRIT_CHANCE
    --print("Created a race named " .. name .. " which has +" .. bonus_roll_att .. " bonus roll while attacking and +" ..  bonus_roll_def .. " bonus roll while defending.")
    return race
 end
